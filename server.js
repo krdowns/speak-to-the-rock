da752fea1fbcd4d349310732f0b167fa0dcc22da
Kenny Downs <kdowns@repairpal.com>
2020-05-07 13:23:37 -0600

    added routes and began serving static files with node.js

---
+++ server.js
@@ -0,0 +1,35 @@
+// REQUIREMENTS
+const express = require('express');
+const app = express();
+const path = require('path')
+
+// MIDDLEWARE
+// app.use('/static', express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ROUTES
+app.get('/', function(req, res) {
+    res.sendFile('./views/homepage.html', {root: __dirname })
+});
+
+app.get('/about', function(req, res) {
+    res.sendFile('./views/about.html', {root: __dirname })
+});
+
+app.get('/buypage', function(req, res) {
+    res.sendFile('./views/buypage.html', {root: __dirname })
+});
+
+app.get('/charge', function(req, res) {
+    res.sendFile('./views/charge.html', {root: __dirname })
+});
+
+app.get('/contact', function(req, res) {
+    res.sendFile('./views/contact.html', {root: __dirname })
+});
+
+
+// SERVER START
+app.listen(3000, () => {
+  console.log("HTTP server listening at localhost:3000");
+});
