8c0a2da65c6211ea4be52c3e521d6e05f9a4432e
Kenny Downs <kdowns@repairpal.com>
2020-05-08 17:14:02 -0600

    added stripe client-side integration, but may revert to external amazon link

--- server.js
+++ server.js
@@ -1,33 +1,36 @@
 // REQUIREMENTS
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path')
+const path = require("path");
 
 // MIDDLEWARE
 // app.use('/static', express.static('public'))
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // ROUTES
-app.get('/', function(req, res) {
-    res.sendFile('./views/homepage.html', {root: __dirname })
+app.get("/", function (req, res) {
+  res.sendFile("./views/homepage.html", { root: __dirname });
 });
 
-app.get('/about', function(req, res) {
-    res.sendFile('./views/about.html', {root: __dirname })
+app.get("/about", function (req, res) {
+  res.sendFile("./views/about.html", { root: __dirname });
 });
 
-app.get('/buypage', function(req, res) {
-    res.sendFile('./views/buypage.html', {root: __dirname })
+app.get("/buypage", function (req, res) {
+  res.sendFile("./views/buypage.html", { root: __dirname });
 });
 
-app.get('/charge', function(req, res) {
-    res.sendFile('./views/charge.html', {root: __dirname })
+app.get("/charge", function (req, res) {
+  res.sendFile("./views/charge.html", { root: __dirname });
 });
 
-app.get('/contact', function(req, res) {
-    res.sendFile('./views/contact.html', {root: __dirname })
+app.get("/contact", function (req, res) {
+  res.sendFile("./views/contact.html", { root: __dirname });
 });
 
+app.get("/secret", async (req, res) => {
+  const intent = res.json({ client_secret: intent.client_secret }); // ... Fetch or create the PaymentIntent
+});
 
 // SERVER START
 app.listen(3000, () => {
