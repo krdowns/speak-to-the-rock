af3f864973f99e28aae674772b8935ad63c9333f
Kenny Downs <kdowns@repairpal.com>
2020-05-16 17:16:45 -0600

    began converting back to single page site with smooth scroll from anchors in nav

--- server.js
+++ server.js
@@ -5,40 +5,40 @@ const path = require("path");
 
 // MIDDLEWARE
 // app.use('/static', express.static('public'))
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
-// ROUTES
-app.get("/", function (req, res) {
-  res.sendFile("./views/homepage.html", { root: __dirname });
-});
+// // ROUTES
+// app.get("/", function (req, res) {
+//   res.sendFile("./views/homepage.html", { root: __dirname });
+// });
 
-app.get("/about", function (req, res) {
-  res.sendFile("./views/about.html", { root: __dirname });
-});
+// app.get("/about", function (req, res) {
+//   res.sendFile("./views/about.html", { root: __dirname });
+// });
 
-app.get("/buypage", function (req, res) {
-  res.sendFile("./views/buypage.html", { root: __dirname });
-});
+// app.get("/buypage", function (req, res) {
+//   res.sendFile("./views/buypage.html", { root: __dirname });
+// });
 
-app.get("/cancelled", function (req, res) {
-  res.sendFile("./views/cancelled.html", { root: __dirname });
-});
+// app.get("/cancelled", function (req, res) {
+//   res.sendFile("./views/cancelled.html", { root: __dirname });
+// });
 
-app.get("/charge", function (req, res) {
-  res.sendFile("./views/charge.html", { root: __dirname });
-});
+// app.get("/charge", function (req, res) {
+//   res.sendFile("./views/charge.html", { root: __dirname });
+// });
 
-app.get("/contact", function (req, res) {
-  res.sendFile("./views/contact.html", { root: __dirname });
-});
+// app.get("/contact", function (req, res) {
+//   res.sendFile("./views/contact.html", { root: __dirname });
+// });
 
-app.get("/secret", async (req, res) => {
-  const intent = res.json({ client_secret: intent.client_secret }); // ... Fetch or create the PaymentIntent
-});
+// app.get("/secret", async (req, res) => {
+//   const intent = res.json({ client_secret: intent.client_secret }); // ... Fetch or create the PaymentIntent
+// });
 
-app.get("/success", function (req, res) {
-  res.sendFile("./views/success.html", { root: __dirname });
-});
+// app.get("/success", function (req, res) {
+//   res.sendFile("./views/success.html", { root: __dirname });
+// });
 
 // SERVER START
 app.listen(3000, () => {
