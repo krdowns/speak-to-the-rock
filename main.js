4def18f4302ae256469ef7d77d31831a24bd40cc
Kenny Downs <kdowns@repairpal.com>
2020-06-07 12:46:36 -0600

    added tumblr embedding, got rid of watch section and made 3 listen sections

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+var playButton = document.getElementById("play_button");
+// Event listener for the play/pause button
+playButton.addEventListener("click", function () {
+  if (video.paused == true) {
+    // Play the video
+    video.play();
+
+    // Update the button text to 'Pause'
+    playButton.innerHTML = "Pause";
+  } else {
+    // Pause the video
+    video.pause();
+
+    // Update the button text to 'Play'
+    playButton.innerHTML = "Play";
+  }
+});
+
 const modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
