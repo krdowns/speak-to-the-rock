7ee85c6cfb2fda03325cb75610693ccaafae8b70
Kenny Downs <kdowns@repairpal.com>
2020-09-02 18:10:47 -0600

    added functions for each individual audio player

--- audioplayer.js
+++ audioplayer.js
@@ -2,15 +2,12 @@ function initProgressBar() {
   const player = document.getElementById("player");
   const length = player.duration;
   const current_time = player.currentTime;
-
   // calculate total length of value
   const totalLength = calculateTotalValue(length);
   document.getElementById("end-time").innerHTML = totalLength;
-
   // calculate current value time
   const currentTime = calculateCurrentValue(current_time);
   document.getElementById("start-time").innerHTML = currentTime;
-
   const progressbar = document.getElementById("seek-obj");
   progressbar.value = player.currentTime / player.duration;
   progressbar.addEventListener("click", seek);
@@ -26,6 +23,52 @@ function initProgressBar() {
   }
 }
 
+function initProgressBar2() {
+  const player2 = document.getElementById("player2");
+  const length2 = player2.duration;
+  const current_time2 = player2.currentTime;
+  const totalLength2 = calculateTotalValue(length2);
+  document.getElementById("end-time2").innerHTML = totalLength2;
+  const currentTime2 = calculateCurrentValue(current_time2);
+  document.getElementById("start-time2").innerHTML = currentTime2;
+  const progressbar2 = document.getElementById("seek-obj2");
+  progressbar2.value = player2.currentTime / player2.duration;
+  progressbar2.addEventListener("click", seek2);
+
+  if (player2.currentTime == player2.duration) {
+    document.getElementById("play-btn2").className = "";
+  }
+
+  function seek2(event) {
+    const percent2 = event.offsetX / this.offsetWidth;
+    player2.currentTime = percent2 * player2.duration;
+    progressbar2.value = percent2 / 100;
+  }
+}
+
+function initProgressBar3() {
+  const player3 = document.getElementById("player3");
+  const length3 = player3.duration;
+  const current_time3 = player3.currentTime;
+  const totalLength3 = calculateTotalValue(length3);
+  document.getElementById("end-time3").innerHTML = totalLength3;
+  const currentTime3 = calculateCurrentValue(current_time3);
+  document.getElementById("start-time3").innerHTML = currentTime3;
+  const progressbar3 = document.getElementById("seek-obj3");
+  progressbar3.value = player3.currentTime / player3.duration;
+  progressbar3.addEventListener("click", seek3);
+
+  if (player3.currentTime == player3.duration) {
+    document.getElementById("play-btn3").className = "";
+  }
+
+  function seek3(event) {
+    const percent3 = event.offsetX / this.offsetWidth;
+    player3.currentTime = percent3 * player3.duration;
+    progressbar3.value = percent3 / 100;
+  }
+}
+
 function initPlayers(num) {
   // pass num in if there are multiple audio players e.g 'player' + i
 
@@ -35,15 +78,34 @@ function initPlayers(num) {
       // ----------------------------------------------------------
       // audio embed object
       let playerContainer = document.getElementById("player-container"),
+        playerContainer2 = document.getElementById("player-container2"),
+        playerContainer3 = document.getElementById("player-container3"),
         player = document.getElementById("player"),
+        player2 = document.getElementById("player2"),
+        player3 = document.getElementById("player3"),
         isPlaying = false,
-        playBtn = document.getElementById("play-btn");
+        playBtn = document.getElementById("play-btn"),
+        playBtn2 = document.getElementById("play-btn2"),
+        playBtn3 = document.getElementById("play-btn3");
 
+      console.log(num);
       // Controls Listeners
       // ----------------------------------------------------------
       if (playBtn != null) {
         playBtn.addEventListener("click", function () {
           togglePlay();
+        });
+      }
+
+      if (playBtn2 != null) {
+        playBtn2.addEventListener("click", function () {
+          togglePlay2();
+        });
+      }
+
+      if (playBtn3 != null) {
+        playBtn3.addEventListener("click", function () {
+          togglePlay3();
         });
       }
 
@@ -58,6 +120,32 @@ function initPlayers(num) {
         } else {
           player.play();
           document.getElementById("play-btn").className = "pause";
+          isPlaying = true;
+        }
+      }
+
+      function togglePlay2() {
+        console.log(document.getElementById("play-btn2").className);
+        if (player2.paused === false) {
+          player2.pause();
+          isPlaying = false;
+          document.getElementById("play-btn2").className = "";
+        } else {
+          player2.play();
+          document.getElementById("play-btn2").className = "pause";
+          isPlaying = true;
+        }
+      }
+
+      function togglePlay3() {
+        console.log(document.getElementById("play-btn3").className);
+        if (player3.paused === false) {
+          player3.pause();
+          isPlaying = false;
+          document.getElementById("play-btn3").className = "";
+        } else {
+          player3.play();
+          document.getElementById("play-btn3").className = "pause";
           isPlaying = true;
         }
       }
@@ -88,4 +176,4 @@ function calculateCurrentValue(currentTime) {
   return current_time;
 }
 
-initPlayers(jQuery("#player-container").length);
+initPlayers(jQuery("#player-container3").length);
--- main.js
+++ main.js
@@ -1,42 +1,42 @@
-var playButton = document.getElementById("play_button");
-// Event listener for the play/pause button
-playButton.addEventListener("click", function () {
-  if (video.paused == true) {
-    // Play the video
-    video.play();
+// var playButton = document.getElementById("play_button");
+// // Event listener for the play/pause button
+// playButton.addEventListener("click", function () {
+//   if (video.paused == true) {
+//     // Play the video
+//     video.play();
 
-    // Update the button text to 'Pause'
-    playButton.innerHTML = "Pause";
-  } else {
-    // Pause the video
-    video.pause();
+//     // Update the button text to 'Pause'
+//     playButton.innerHTML = "Pause";
+//   } else {
+//     // Pause the video
+//     video.pause();
 
-    // Update the button text to 'Play'
-    playButton.innerHTML = "Play";
-  }
-});
+//     // Update the button text to 'Play'
+//     playButton.innerHTML = "Play";
+//   }
+// });
 
-const modal = document.getElementById("myModal");
+// const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-const btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// const btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
+// // When the user clicks on the button, open the modal
+// btn.onclick = function () {
+//   modal.style.display = "block";
+// };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
