dd8e46bca5842e46883e0f019fe599e1b107129d
Kenny Downs <kdowns@repairpal.com>
2021-02-07 18:51:03 -0700

    adding listen page

--- server.js
+++ server.js
@@ -21,4 +21,61 @@ app.get("/watch", function (req, res) {
   res.sendFile(path.join(__dirname + "/watch.html"));
 });
 
+// Set your secret key. Remember to switch to your live secret key in production!
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+const Stripe = require("stripe");
+const stripe = Stripe("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+
+// const product = await stripe.products.create({
+//   name: "Job Unseen",
+//   images: ["./styles/media/job-unseen.jpg"],
+//   id: "prod_1",
+//   shippable: true,
+// });
+
+// const price = await stripe.prices.create({
+//   product: {product.id},
+//   unit_amount: 1999,
+//   currency: "usd",
+// });
+
+app.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    billing_address_collection: "auto",
+    shipping_address_collection: {
+      allowed_countries: ["US", "CA"],
+    },
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: "Job Unseen Paperback",
+          },
+          unit_amount: 1949,
+        },
+        quantity: 1,
+      },
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: "Job Unseen E-Book",
+          },
+          unit_amount: 999,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: "https://example.com/success",
+    cancel_url: "https://example.com/cancel",
+  });
+
+  res.json({ id: session.id });
+});
+
+app.listen(4242, () => console.log(`Listening on port ${4242}!`));
+
 app.listen(process.env.PORT || 8080);
